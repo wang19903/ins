882d2687743349f89065da21f0afc2e9c1249fb8
wangg <lf21715@yahoo.com.tw>
2023-08-14 14:33:38 +0800

    clean

---
+++ frontend/src/routes.js
@@ -0,0 +1,51 @@
+import HomePage from "./pages/HomePage.vue";
+import SearchPage from "./pages/SearchPage.vue";
+import ProfilePage from "./pages/ProfilePage.vue";
+import ProfileEdittingPage from "./pages/ProfileEdittingPage.vue";
+import LoginPage from "./pages/LoginPage.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import { getJwtToken } from "./apis/auth";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomePage
+  },
+  {
+    path: "/search_result",
+    name: "search_result",
+    component: SearchPage
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    component: ProfilePage
+  },
+  {
+    path: "/profile/edit",
+    name: "profileEdit",
+    component: ProfileEdittingPage
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: LoginPage
+  }
+];
+
+const router = createRouter({
+  routes: routes,
+  history: createWebHistory()
+});
+
+router.beforeEach((to) => {
+  if (to.name !== "login" && !getJwtToken()) {
+    return { name: "login" };
+  }
+  if (to.name === "login" && getJwtToken()) {
+    return { name: "home" };
+  }
+});
+
+export { router }; //命名導出
---
+++ frontend/src/store/comment/index.js
@@ -0,0 +1,25 @@
+import { createComment, loadComments } from "../../apis/comment";
+
+export const comment = {
+  state() {
+    return {
+      list: [],
+    };
+  },
+  mutations: {
+    initializeComments(state, comments) {
+      state.list = comments;
+    },
+  },
+  actions: {
+    async addComment({ commit, dispatch }, { content, postId }) {
+      await createComment(content, postId);
+      dispatch("loadAllComments", postId);
+      commit("increaseCommentCount", postId);
+    },
+    async loadAllComments({ commit }, postId) {
+      const comments = await loadComments(postId);
+      commit("initializeComments", comments);
+    },
+  },
+};
